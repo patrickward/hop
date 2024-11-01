b61d99a22fba9b5ea8e0a6980cf463ef179bfdfa
Patrick Ward <patrick@octetic.com>
2024-11-01 16:33:35 -0400

    don't override env and debug from commandline

--- conf/flagset.go
+++ conf/flagset.go
@@ -24,17 +24,17 @@ func NewFlagSet(appName string, errorHandling flag.ErrorHandling) *flag.FlagSet 
 // BasicFlags sets up the common flags most apps will need
 func BasicFlags(fs *flag.FlagSet) {
 	fs.String("config", "config.json", "Path to config file")
-	fs.String("env", "development", "Environment (development, staging, production)")
-	fs.Bool("debug", false, "Enable debug mode")
 	fs.Bool("version", false, "Show version and exit")
+	//fs.String("env", "development", "Environment (development, staging, production)")
+	//fs.Bool("debug", false, "Enable debug mode")
 }
 
 // ApplyFlagOverrides ensures that if a basic flag is set, it overrides the config
 func ApplyFlagOverrides(cfg *BaseConfig, fs *flag.FlagSet) {
-	if fs.Lookup("debug").Value.String() == "true" {
-		cfg.App.Debug = true
-	}
-	if fs.Lookup("env").Value.String() != "" {
-		cfg.App.Environment = fs.Lookup("env").Value.String()
-	}
+	//if fs.Lookup("debug").Value.String() == "true" {
+	//	cfg.App.Debug = true
+	//}
+	//if fs.Lookup("env").Value.String() != "" {
+	//	cfg.App.Environment = fs.Lookup("env").Value.String()
+	//}
 }
